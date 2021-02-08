d48a0d0785462893cc7b0abd50c67f8613cec6d7
Jarvis Kennedy <jarvis.k18@gmail.com>
2021-02-08 14:10:05 -0500

    still testing

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import sqlite3
 import requests
 import re
 
-conn = sqlite3.connect('courses.db')
+conn = sqlite3.connect('degree-helper/courses.db')
 db = conn.cursor()
 
 
 
-
+index = 'https://student.utm.utoronto.ca/calendar'
 url = 'https://student.utm.utoronto.ca/calendar/depart_list.pl'
 page = requests.get(url)
 
@@ -16,7 +16,11 @@ soup = bs4.BeautifulSoup(page.content, 'html.parser')
 pages = soup.find_all('a', href = re.compile('newdep'))
 
 for page in pages:
-    print(page.get_text())
+    if page.get_text() == 'Mathematics':
+        math_page = requests.get(index + '/' + page.get('href'))
+        math_soup = bs4.BeautifulSoup(math_page.content, 'html.parser')
+
+print(math_soup.prettify())
 
 
 conn.commit()
