42aee19d8aea2ecf104991977da56d9b7c2b4621
Jarvis Kennedy <jarvis.k18@gmail.com>
2021-02-08 13:27:43 -0500

    testing

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("this is a python file")
+import bs4
+import sqlite3
+import requests
+import re
+
+conn = sqlite3.connect('courses.db')
+db = conn.cursor()
+
+
+
+
+url = 'https://student.utm.utoronto.ca/calendar/depart_list.pl'
+page = requests.get(url)
+
+soup = bs4.BeautifulSoup(page.content, 'html.parser')
+pages = soup.find_all('a', href = re.compile('newdep'))
+
+for page in pages:
+    print(page.get_text())
+
+
+conn.commit()
+conn.close()
+
+print('Program ran okay')
+
+
